1ad2193e2ae7b5f1db9f55001ac8d04b1da0db74
sunsheng.zhu <sunsheng.zhu@moco.com>
2021-10-13 11:44:16 +0800

    .

--- src/main/java/com/jna/core/JnaHotKey.java
+++ src/main/java/com/jna/core/JnaHotKey.java
@@ -1,6 +1,5 @@
 package com.jna.core;
 
-import java.util.Collection;
 import java.util.function.Consumer;
 
 import com.sun.jna.platform.win32.BaseTSD;
@@ -18,7 +17,26 @@ import com.sun.jna.platform.win32.WinUser.INPUT;
  */
 public class JnaHotKey {
 
-	//ALT+F4
+	//KeyS
+	public static synchronized void combinKeyInput(long keys) {
+		INPUT[] ipArr = (INPUT[])(new INPUT()).toArray(2);
+		arrayset(ipArr, ip -> {ip.type = new WinDef.DWORD(WinUser.INPUT.INPUT_KEYBOARD);});
+		
+		arrayset(ipArr, ip -> {ip.input.setType("ki");});
+		arrayset(ipArr, ip -> {ip.input.ki.wScan = new WinDef.WORD(0);});
+		arrayset(ipArr, ip -> {ip.input.ki.time = new WinDef.DWORD(0);});
+		arrayset(ipArr, ip -> {ip.input.ki.dwExtraInfo = new BaseTSD.ULONG_PTR(0);});
+		
+		arrayset(ipArr, new int[] {0, 1}, ip -> {ip.input.ki.wVk = new WinDef.WORD(keys);});  //keys
+		
+		arrayset(ipArr, new int[] {0}, ip -> {ip.input.ki.dwFlags = new WinDef.DWORD(0);});//keydown
+		arrayset(ipArr, new int[] {1}, ip -> {ip.input.ki.dwFlags = new WinDef.DWORD(2);});//keyup
+		
+		if (ipArr == null || ipArr.length == 0) return;
+        User32.INSTANCE.SendInput(new WinDef.DWORD(ipArr.length), ipArr, ipArr[0].size());
+    }
+	
+	//Ctrl+KeyS
 	public static synchronized void combinKeyInput(long ctrl, long keys) {
 		INPUT[] ipArr = (INPUT[])(new INPUT()).toArray(4);
 		arrayset(ipArr, ip -> {ip.type = new WinDef.DWORD(WinUser.INPUT.INPUT_KEYBOARD);});
@@ -68,6 +86,8 @@ public class JnaHotKey {
 					Thread.sleep(5*1000);
 					
 					//combinKeyInput(162, 86);//Ctrl + V
+					combinKeyInput(65);//a
+					combinKeyInput(160, 65);//Shift + a -> A
 					combinKeyInput(91, 82);//Win + R
 					
 					Thread.sleep(1*1000);
